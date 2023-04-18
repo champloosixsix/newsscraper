afaf7c5d2574ad957741bcde79f284b3cbf9a5dd
champloosixsix <seanlovins@gmail.com>
2023-04-18 19:57:55 -0400

    First commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import * as mdb from 'mdb-ui-kit';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
